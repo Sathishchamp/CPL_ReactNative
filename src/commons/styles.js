2cb6d1387c269bccfbb75f2cd7a3c2f8e831f3c0
Ramu Ramasamy <rram.ramasamy@gmail.com>
2019-06-19 14:20:41 +0530

    header bg color changed

--- src/commons/styles.js
+++ src/commons/styles.js
@@ -3,7 +3,7 @@ import { PRIMARY, ON_PRIMARY, VIEW_BG_COLOR, TAB_BG } from '../config/colors';
 
 export default StyleSheet.create({
   headerDefault: {
-    backgroundColor: TAB_BG,
+    backgroundColor: VIEW_BG_COLOR,
     // shadowColor: 'grey',
     shadowOffset: { width: 1, height: 1 },
     shadowOpacity: 0.5,
