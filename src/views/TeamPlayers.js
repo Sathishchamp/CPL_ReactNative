8b7c4018b0946563db116abe4c01432139a4c0de
Ramu Ramasamy <rram.ramasamy@gmail.com>
2019-07-16 07:56:33 +0530

    coach and assistant view

--- src/views/TeamPlayers.js
+++ src/views/TeamPlayers.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { View, StyleSheet, FlatList } from 'react-native';
+import {
+  View,
+  StyleSheet,
+  FlatList,
+  Dimensions,
+  Image,
+  Text
+} from 'react-native';
 import { Container, Content } from 'native-base';
 import commonStyles from '../commons/styles';
 import APIService from '../services/APIService';
@@ -17,6 +24,10 @@ import PlayerCard from '../components/PlayerCard';
 import { isEqual } from '../utils';
 import Spinner from 'react-native-loading-spinner-overlay';
 import { isNullOrEmpty } from '../utils';
+import { VAGROUND, HELVETICA } from '../constants/fonts';
+
+const height = Dimensions.get('window').height * 0.22;
+const width = Dimensions.get('window').width;
 
 class TeamPlayers extends React.PureComponent {
   constructor(props) {
@@ -24,7 +35,8 @@ class TeamPlayers extends React.PureComponent {
     this.state = {
       spinner: false,
       playerProfiles: [],
-      backgroundColor: null
+      backgroundColor: null,
+      coachDetails: {}
     };
   }
 
@@ -59,7 +71,18 @@ class TeamPlayers extends React.PureComponent {
               });
             }
             console.log(playerProfiles);
-            this.setState({ playerProfiles, backgroundColor, spinner: false });
+            const coachDetails = {
+              coachImage: data.coachimage,
+              coachName: data.coachname,
+              assistantImage: data.assistantimage,
+              assistantName: data.assistantname
+            };
+            this.setState({
+              playerProfiles,
+              backgroundColor,
+              spinner: false,
+              coachDetails
+            });
           } else {
             this.setState({ spinner: false });
           }
@@ -93,11 +116,69 @@ class TeamPlayers extends React.PureComponent {
     );
   }
 
+  _renderCoachDetailsView(title, uri, name) {
+    return (
+      <View style={styles.coachDetailsMainView}>
+        <View style={styles.coachTitleView}>
+          <Text style={styles.coachTitleText}>{title}</Text>
+        </View>
+        <View style={styles.coachDetailsInnerView}>
+          <View style={styles.coachImageView}>
+            <Image
+              source={{
+                uri
+              }}
+              resizeMode="contain"
+              style={styles.coachImage}
+            />
+          </View>
+          <View style={styles.coachNameView}>
+            <Text style={styles.coachNameText}>{name}</Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
+
+  _renderCoachView() {
+    const teamImage = this.props.navigation.getParam('teamImage');
+    const { coachDetails } = this.state;
+    return (
+      <View
+        style={[
+          styles.coachView,
+          {
+            backgroundColor: this.state.backgroundColor
+          }
+        ]}
+      >
+        <View style={styles.teamImageView}>
+          <Image
+            source={{ uri: teamImage }}
+            style={styles.teamImage}
+            resizeMode="contain"
+          />
+        </View>
+        {this._renderCoachDetailsView(
+          'COACH',
+          coachDetails.coachImage,
+          coachDetails.coachName
+        )}
+        {this._renderCoachDetailsView(
+          'ASSISTANT',
+          coachDetails.assistantImage,
+          coachDetails.assistantName
+        )}
+      </View>
+    );
+  }
+
   render() {
     return (
       <Container>
-        <Content padder style={commonStyles.content}>
-          <View>
+        <Content style={commonStyles.content}>
+          {this._renderCoachView()}
+          <View style={{ padding: 10 }}>
             <FlatList
               data={this.state.playerProfiles}
               renderItem={({ item }) => (
@@ -122,3 +203,67 @@ const mapStateToProps = state => ({
 });
 
 export default connect(mapStateToProps)(TeamPlayers);
+
+const styles = StyleSheet.create({
+  coachView: {
+    height,
+    width,
+    flexDirection: 'row'
+  },
+  teamImageView: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  teamImage: {
+    height: height * 0.8,
+    width: height * 0.8
+  },
+  coachDetailsMainView: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  coachTitleView: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 4
+  },
+  coachTitleText: {
+    fontFamily: VAGROUND,
+    color: '#ffffff'
+  },
+  coachDetailsInnerView: {
+    flex: 10,
+    flexDirection: 'column',
+    backgroundColor: '#ffffff',
+    borderRadius: 10,
+    margin: 5,
+    marginBottom: 10
+  },
+  coachImageView: {
+    flex: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#940a00'
+  },
+  coachImage: {
+    height: height * 0.62,
+    width: height * 0.62
+  },
+  coachNameView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  coachNameText: {
+    fontFamily: HELVETICA,
+    fontWeight: '500'
+  }
+});
